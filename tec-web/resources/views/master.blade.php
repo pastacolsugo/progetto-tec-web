1de24233861d79a61b6263e4bc2e27dd2ff5d2bc
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2021-12-02 08:44:33 +0100

    add things

---
+++ tec-web/resources/views/master.blade.php
@@ -0,0 +1,26 @@
+<main>
+
+    
+
+    <!-- table -->
+    <section id="intro-view">
+        <div class="container py-4">
+            <div class="row">
+
+                <!-- table left -->
+                <div class="col">
+                    <h1>eCommer</h1>
+                    <p>Ciao</p>
+                </div>
+
+                <!-- table right -->
+                <div class="col">
+                    <h3>Magari un video</h3>
+                    <p>Ciao</p>
+                </div>
+            </div>
+        </div>      
+    </section>
+
+
+</main>
