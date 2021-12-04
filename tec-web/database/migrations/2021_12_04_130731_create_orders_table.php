94ec6d364ca0e4b708740c3fef76ae25aff5feea
alicegirolomini <alice.girolomini@studio.unibo.it>
2021-12-04 14:44:19 +0100

    added order model and create_orders_table

---
+++ tec-web/database/migrations/2021_12_04_130731_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string("ship_address");
+            $table->date('order_date');
+            $table->date('shipped_date');
+            $table->floatval('order_total');
+            $table->string("order_status");
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
