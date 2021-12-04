a3b2beed3c2448712d4c4a9e990d3b081f215580
alicegirolomini <alice.girolomini@studio.unibo.it>
2021-12-04 14:49:26 +0100

    added orderItem model and create_order_items_table

---
+++ tec-web/database/migrations/2021_12_04_134556_create_order_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('product');
+            $table->foreign('order_id')->references('id')->on('order');
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
