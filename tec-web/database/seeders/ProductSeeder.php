5b9c54732828a4237c1f1412e1c50f37ae949b27
alicegirolomini <alice.girolomini@studio.unibo.it>
2021-12-14 18:25:10 +0100

    Added images in products

--- tec-web/database/seeders/ProductSeeder.php
+++ tec-web/database/seeders/ProductSeeder.php
@@ -21,7 +21,8 @@ class ProductSeeder extends Seeder
                 'description'=>'A smartphone with 8gb ram and much more feature',
                 'stock'=>'100',
                 'category_id'=>'1',
-                'seller_id'=>'3'
+                'seller_id'=>'3',
+                'gallery'=>'https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBOPPO-A52-6-GFUTU6297453D3D253C/1592019058170_0..png'
             ],
             [
                 'name'=>'Panasonic Tv',
@@ -29,15 +30,17 @@ class ProductSeeder extends Seeder
                 'description'=>'A smart tv with much more feature',
                 'stock'=>'10',
                 'category_id'=>'2',
-                'seller_id'=>'3'
+                'seller_id'=>'3',
+                'gallery'=>'https://www.panasonic.com/content/dam/pim/it/it/TX/TX-40D/TX-40DS400E/TX-40DS400E-Variation_Image_for_See_All_1Global-1_it_it.png'
             ],
             [
-                'name'=>'Soni Tv',
+                'name'=>'Sony Tv',
                 'price'=>'850',
                 'description'=>'A tv with much more feature',
                 'stock'=>'5',
                 'category_id'=>'2',
-                'seller_id'=>'3'
+                'seller_id'=>'3',
+                'gallery'=>'https://4.imimg.com/data4/PM/KH/MY-34794816/lcd-500x500.png'
             ],
             [
                 'name'=>'LG fridge',
@@ -45,7 +48,8 @@ class ProductSeeder extends Seeder
                 'description'=>'A fridge with much more feature',
                 'stock'=>'250',
                 'category_id'=>'3',
-                'seller_id'=>'3'
+                'seller_id'=>'3',
+                'gallery'=>'https://www.lg.com/us/images/refrigerators/md05966856/gallery/D-01.jpg'
              ]
         ]);
     }
