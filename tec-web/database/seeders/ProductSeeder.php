39d0897a4dcbb7a3e64c373a2c2449d2261c2882
alicegirolomini <alice.girolomini@studio.unibo.it>
2021-12-14 15:13:52 +0100

    Added ProductSeeder

---
+++ tec-web/database/seeders/ProductSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            [
+                'name'=>'Oppo mobile',
+                'price'=>'350',
+                'description'=>'A smartphone with 8gb ram and much more feature',
+                'stock'=>'100',
+                'category_id'=>'1',
+                'seller_id'=>'3'
+            ],
+            [
+                'name'=>'Panasonic Tv',
+                'price'=>'499.99',
+                'description'=>'A smart tv with much more feature',
+                'stock'=>'10',
+                'category_id'=>'2',
+                'seller_id'=>'3'
+            ],
+            [
+                'name'=>'Soni Tv',
+                'price'=>'850',
+                'description'=>'A tv with much more feature',
+                'stock'=>'5',
+                'category_id'=>'2',
+                'seller_id'=>'3'
+            ],
+            [
+                'name'=>'LG fridge',
+                'price'=>'200',
+                'description'=>'A fridge with much more feature',
+                'stock'=>'250',
+                'category_id'=>'3',
+                'seller_id'=>'3'
+             ]
+        ]);
+    }
+}
