ad3649a874fa723972b2f234d99b214015d8afa9
alicegirolomini <alice.girolomini@studio.unibo.it>
2021-12-14 15:13:30 +0100

    Added CategorySeeder

---
+++ tec-web/database/seeders/CategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            [
+                'name'=>'Mobile'
+            ],
+            [
+                'name'=>'Tv'
+            ],
+            [
+                'name'=>'Fridge'
+             ]
+        ]);
+    }
+}
