0e9128014990697fd4962f59be2c32a1de32eae4
alicegirolomini <alice.girolomini@studio.unibo.it>
2021-12-14 15:13:03 +0100

    Added UserSeeder

---
+++ tec-web/database/seeders/UserSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name'=>'Peter Parker',
+                'email'=>'peter@parker.com',
+                'password'=>Hash::make('12345')
+            ],
+            [
+                'name'=>'Bruce Banner',
+                'email'=>'bruce@banner.com',
+                'password'=>Hash::make('12345')
+            ],
+            [
+                'name'=>'Tony Stark',
+                'email'=>'tony@stark.com',
+                'password'=>Hash::make('12345')
+            ],
+            [
+                'name'=>'Steve Rogers',
+                'email'=>'steve@rogers.com',
+                'password'=>Hash::make('12345')
+            ],
+            [
+                'name'=>'Natasha Romanoff',
+                'email'=>'natasha@romanoff.com',
+                'password'=>Hash::make('12345')
+            ]
+        ]);
+    }
+}
