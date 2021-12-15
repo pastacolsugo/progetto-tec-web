a7474b44fccc4e735eb3511b44c1deae8275c985
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2021-12-15 11:10:10 +0100

    create products
    simple demo

--- tec-web/resources/views/products.blade.php
+++ tec-web/resources/views/products.blade.php
@@ -1,6 +1,6 @@
 <head>
     <!-- css -->
-    <link rel="stylesheet" href="/css/login.css">
+    <link rel="stylesheet" href="/css/products.css">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -14,5 +14,90 @@
     {{View::make('header')}}
     @yield('content')
 
-    
+
+    <main>
+        <table>
+            <div class="row">
+                <div class="col-3">
+
+                </div>
+                <div class="col-9">
+                    <!-- products -->
+                    <div class="container-lg py-3">
+                        <div class="row">
+                            <div class="col">
+                                <div class="card" style="width: 15rem;">
+                                    <img src="/img/iphone.jpeg" class="card-img-top" alt="">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Apple iPhone 13 Pro Max (512 GB) - Azzurro Sierra</h5>
+                                        <p class="card-text">Prezzo: 1.639,00$</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card" style="width: 15rem;">
+                                    <img src="/img/iphone.jpeg" class="card-img-top" alt="">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Apple iPhone 13 Pro Max (512 GB) - Azzurro Sierra</h5>
+                                        <p class="card-text">Prezzo: 1.639,00$</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card" style="width: 15rem;">
+                                    <img src="/img/iphone.jpeg" class="card-img-top" alt="">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Apple iPhone 13 Pro Max (512 GB) - Azzurro Sierra</h5>
+                                        <p class="card-text">Prezzo: 1.639,00$</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container-lg py-3">
+                        <div class="row">
+                            <div class="col">
+                                <div class="card" style="width: 15rem;">
+                                    <img src="/img/iphone.jpeg" class="card-img-top" alt="">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Apple iPhone 13 Pro Max (512 GB) - Azzurro Sierra</h5>
+                                        <p class="card-text">Prezzo: 1.639,00$</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card" style="width: 15rem;">
+                                    <img src="/img/iphone.jpeg" class="card-img-top" alt="">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Apple iPhone 13 Pro Max (512 GB) - Azzurro Sierra</h5>
+                                        <p class="card-text">Prezzo: 1.639,00$</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card" style="width: 15rem;">
+                                    <img src="/img/iphone.jpeg" class="card-img-top" alt="">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Apple iPhone 13 Pro Max (512 GB) - Azzurro Sierra</h5>
+                                        <p class="card-text">Prezzo: 1.639,00$</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </table>
+
+    </main>
+
+    <!-- footer -->
+    {{View::make('footer')}}
+    @yield('content')
 </body>
