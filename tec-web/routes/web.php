c70bbfdd7d53b99bb7aeb882763fbc4082c6d2a4
Ugo <ugobaroncini@gmail.com>
2021-12-16 14:15:48 +0100

    Restored product view

--- tec-web/routes/web.php
+++ tec-web/routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view("/products", 'products');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
