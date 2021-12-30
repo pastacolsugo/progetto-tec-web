ae8e3c79c4e12dfb41fa68cf9a9a80ab96103a95
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2021-12-30 19:01:06 +0100

    add

--- tec-web/resources/views/master.blade.php
+++ tec-web/resources/views/master.blade.php
@@ -1,24 +1,42 @@
 <main>
 
-    <!-- first-view -->
-    <section id="first-view">
-        <div class="container">
-					<div class="row">
-						<div class="col">
-							<div>
-								<p class="h1">
-									Get what you want!
-								</p>
-								<p class="h2 text-muted">
-								The easiest and fastest way to sell and buy what you want.
-								</p>
-							</div>
-						</div>
-						<div class="col">
-							
-						</div>
+	<!-- 1-view: carusel -->
+	<section id="first-view">
+		<div class="container py-2">
+			<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+				<div class="carousel-inner">
+					<div class="carousel-item active" data-bs-interval="10000">
+						<img src="./img/space1.jpeg" class="d-block w-100" alt="" />
 					</div>
-        </div>
-    </section>
+					<div class="carousel-item" data-bs-interval="2000">
+						<img src="./img/space2.jpeg" class="d-block w-100" alt="" />
+					</div>
+					<div class="carousel-item">
+						<img src="./img/space3.jpeg" class="d-block w-100" alt="" />
+					</div>
+				</div>
+				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Previous</span>
+				</button>
+				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Next</span>
+				</button>
+			</div>
+		</div>
+	</section>
+
+	<!-- 2-view: top-choise-->
+	<section id="second-view">
+		<div class="container py-2">
+
+		</div>
+	</section>
+
+	<!-- 3-view: last-view-products -->
+	<section id="third-view">
+
+	</section>
 
 </main>
