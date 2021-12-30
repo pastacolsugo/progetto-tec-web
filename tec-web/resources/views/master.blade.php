7cad7f5a76920c7967069529e6404dbd4a120f82
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2021-12-30 11:16:38 +0100

    change main

--- tec-web/resources/views/master.blade.php
+++ tec-web/resources/views/master.blade.php
@@ -1,75 +1,24 @@
 <main>
-    <!-- view 1 -->
-    <div class="container-lg py-3">
-        <div class="row">
-            <div class="col py-2">
-                <h2 class="display-3 text-center text-md-start" id="main-title">
-                    Compra quello che vuoi, quando vuoi, dove vuoi...
-                </h2>
-                <h3 class="lead text-center text-md-start text-muted mb-6 mb-lg-8 py-2">
-                    eCommerce è il sito più semplice per acquistare e vendere quello che ti serve.
-                    Registrati per iniziare oggi stesso.
-                </h3>
-            </div>
-            <div class="col py-2">
-                <div class="card">
-                    <img src="img/girl-gacd840e76_1920.jpg" class=" card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <!-- view 2 -->
-    <div class="container py-5">
-        <h2>
-            Confronta i servizi:
-        </h2>
-        <div class="row" id="table-service">
-            <div class="col">
-                <div class="card" id="view-standard">
-                    <div class="card-view">
-                        <h2 class="service-name-item">
-                            Standard
-                        </h2>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Standard</h5>
-                        <p class="card-text">Piano standard: 0$/m</p>
-                        <a href="#" class="btn btn-primary">Go</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card"  id="view-premium">
-                    <div class="card-view">
-                        <h2 class="service-name-item">
-                            Premium
-                        </h2>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Premium</h5>
-                        <p class="card-text">Piano premium: 9.99$/m</p>
-                        <a href="#" class="btn btn-primary">Go</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card" id="view-business">
-                    <div class="card-view">
-                        <h2 class="service-name-item">
-                            For Business
-                        </h2>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">For business</h5>
-                        <p class="card-text">Piano premium: 29.99$/m</p>
-                        <a href="#" class="btn btn-primary">Go</a>
-                    </div>
-                </div>
-            </div>
+    <!-- first-view -->
+    <section id="first-view">
+        <div class="container">
+					<div class="row">
+						<div class="col">
+							<div>
+								<p class="h1">
+									Get what you want!
+								</p>
+								<p class="h2 text-muted">
+								The easiest and fastest way to sell and buy what you want.
+								</p>
+							</div>
+						</div>
+						<div class="col">
+							
+						</div>
+					</div>
         </div>
-    </div>
+    </section>
+
 </main>
