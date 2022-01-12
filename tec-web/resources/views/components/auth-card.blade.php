d98cab24b958399d4a2bb961cff92e6ca03dc62a
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2022-01-12 19:36:35 +0100

    Update auth-card.blade.php

--- tec-web/resources/views/components/auth-card.blade.php
+++ tec-web/resources/views/components/auth-card.blade.php
@@ -1,3 +1,4 @@
+<!-- 
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
     <div>
         {{ $logo }}
@@ -5,5 +6,16 @@
 
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
+    </div>
+</div>
+-->
+
+<div class="card">
+    <img src="" class="card-img-top" alt="" />
+    <div class="card-body">
+        <h5 class="card-title">Nome prodotto</h5>
+        <p class="card-text">Descrizione</p>
+        <p class="card-text">Prezzo: </p>
+        <a href="#" class="btn btn-primary">Acquista ora</a>
     </div>
 </div>
