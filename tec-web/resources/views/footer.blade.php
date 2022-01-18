5c8ffd369e397abef1e6d048cb13e5db8029ab38
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2022-01-18 10:47:57 +0100

    remove things

--- tec-web/resources/views/footer.blade.php
+++ tec-web/resources/views/footer.blade.php
@@ -1,46 +1,10 @@
-<div class="container">
-  <footer class="py-3 my-4">
-    <div class="bg-gray-100 text-dark-700 py-6 text-center">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4 service-column">
-            <svg class="svg-icon service-icon">
-              <use xlink:href="#delivery-time-1"> </use>
-            </svg>
-            <div class="service-text">
-              <h6 class="text-uppercase">Free shipping &amp; return</h6>
-              <p class="text-muted fw-light text-sm mb-0">Free Shipping over $30</p>
-            </div>
-          </div>
-          <div class="col-lg-4 service-column">
-            <svg class="svg-icon service-icon">
-              <use xlink:href="#customer-support-1"> </use>
-            </svg>
-            <div class="service-text">
-              <h6 class="text-uppercase">Money back guarantee</h6>
-              <p class="text-muted fw-light text-sm mb-0">30 Days Money Back Guarantee</p>
-            </div>
-          </div>
-          <div class="col-lg-4 service-column">
-            <svg class="svg-icon service-icon">
-              <use xlink:href="#customer-support-1"> </use>
-            </svg>
-            <div class="service-text">
-              <h6 class="text-uppercase">012-345-6789</h6>
-              <p class="text-muted fw-light text-sm mb-0">24/7 Available Support</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3 py-4">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-    </ul>
-    <p class="text-center text-muted">© 2021 eCommerce, Inc</p>
-  </footer>
+<ul class="nav justify-content-center border-bottom pb-3 mb-3 py-4">
+  <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+  <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+  <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+  <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+  <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+</ul>
+<p class="text-center text-muted">© 2021 eCommerce, Inc</p>
+</footer>
 </div>
