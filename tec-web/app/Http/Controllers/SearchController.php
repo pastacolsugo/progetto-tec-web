6b14e741e2f0c6bcce622e558a1d720ccefdedd5
alicegirolomini <alice.girolomini@studio.unibo.it>
2022-01-19 18:04:45 +0100

    modified search controller

--- tec-web/app/Http/Controllers/SearchController.php
+++ tec-web/app/Http/Controllers/SearchController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class SearchController extends Controller
 {
-    public function __invoke()
+
+    public function __invoke(Request $request)
     {
-        return view('search');
+        $results = null;
+
+        if($searchQuery = $request->get('searchQuery')){
+            $results = Product::search($searchQuery)->get();
+        }
+
+        return view('search', ['results' => $results]);
     }
 }
--- tec-web/resources/views/search.blade.php
+++ tec-web/resources/views/search.blade.php
@@ -1,17 +1,10 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Search') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    Search
-                </div>
-            </div>
-        </div>
+@if($results)
+    <div class="space-y-4">
+        @if($results->count())
+            @foreach($results as $result)
+            @endforeach
+        @else
+            <p>No results found</p>
+        @endif
     </div>
-</x-app-layout>
+@endif
