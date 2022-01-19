efd69a466ca189407cd9ffdf15feabf4d425c6ce
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2022-01-19 13:46:46 +0100

    fix route for products

--- tec-web/routes/web.php
+++ tec-web/routes/web.php
@@ -19,17 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/products', 'products', function () {
-
-//     $products = \App\Models\Product::all();
-
-//     dd($products);
-
-//     return view('products', ['products' => $products]);
-// });
-
-Route::view("/products", 'products');
-
 Route::get('/search', SearchController::class);
 
 Route::get('/dashboard', function () {
