9f4f634c32da14c802321628710220db03f9faf8
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2022-01-19 13:49:59 +0100

    Update products.blade.php

--- tec-web/resources/views/products.blade.php
+++ tec-web/resources/views/products.blade.php
@@ -1,28 +1,26 @@
-<head>
-    <!-- css -->
-    <link rel="stylesheet" href="/css/products.css">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <!-- Bootstrap CSS -->
+<head>
+    <link rel="stylesheet" href="/css/products.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 </head>
 
 <body>
-    <!-- scprit bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    {{View::make('header')}}
-    @yield('content')
+    {{ View::make('header') }}
 
+   <div class="card-grid">
+        @foreach ($products as $product)
+            <x-product-card>
+              <x-slot name="productName">{{ $product->name }}</x-slot>
+              <x-slot name="productGallery"> {{ $product->gallery }}</x-slot>
+              <x-slot name="productDescription"> {{ $product->description}}</x-slot>
+              <x-slot name="productPrice"> {{ $product->price}}</x-slot>
+            </x-product-card>
+        @endforeach
+    </div> 
 
-    <main>
-        <div class="card-grid">
-            
-        
-        </div>
-    </main>
-
-    <!-- footer -->
-    {{View::make('footer')}}
-    @yield('content')
 </body>
+
+</html>
