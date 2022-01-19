127c9182f44339ed4bb70c192483954c7e24210a
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2022-01-19 15:29:42 +0100

    fix products route

--- tec-web/routes/web.php
+++ tec-web/routes/web.php
@@ -25,6 +25,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::resource('product', ProductController::class);
+Route::resource('products', ProductController::class);
 
 require __DIR__.'/auth.php';
