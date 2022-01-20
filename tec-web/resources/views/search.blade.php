a150b3f3092fd250241099322ceb9e874944cb38
Ugo <ugobaroncini@gmail.com>
2022-01-20 14:54:19 +0100

    Proof of concept product search view

--- tec-web/resources/views/search.blade.php
+++ tec-web/resources/views/search.blade.php
@@ -2,6 +2,10 @@
     <div class="space-y-4">
         @if($results->count())
             @foreach($results as $result)
+                <p>{{ $result->name }}</p>
+                <!-- Proof of concept -->
+                <!-- Basic way to show search results -->
+                <!-- @Alice Feel free to replace with more complete version -->
             @endforeach
         @else
             <p>No results found</p>
