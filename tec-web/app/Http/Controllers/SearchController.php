9b93e4b8491bd55dbd601894cf56e4f43edd0be3
alicegirolomini <alice.girolomini@studio.unibo.it>
2022-01-28 13:50:02 +0100

    Added orderBy and pagination to search controller

--- tec-web/app/Http/Controllers/SearchController.php
+++ tec-web/app/Http/Controllers/SearchController.php
@@ -13,9 +13,24 @@ class SearchController extends Controller
         $results = null;
 
         if($query = $request->get('query')){
-            $results = Product::search($query)->paginate(2);
+           // $results = Product::search($query)->paginate(10);
+
+            switch (request('sortBy')) {
+                case 'ascendingPrice':
+                    $results = Product::search($query)->get()->pluck('price');
+                    $results= Product::whereIn('price',$results)->orderBy('price','asc')->paginate(10);
+                    break;
+                case 'descendingPrice':
+                    $results = Product::search($query)->get()->pluck('price');
+                    $results= Product::whereIn('price',$results)->orderBy('price','desc')->paginate(10);
+                    break;
+                default:
+                    $results = Product::search($query)->paginate(10);;
+                    break;
+            }
         }
 
         return view('search', ['results' => $results]);
     }
+
 }
