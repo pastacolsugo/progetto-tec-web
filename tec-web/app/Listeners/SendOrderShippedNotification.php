673d9a4d8a04faa8e81ddf6a8193bf8dbe2d981f
alicegirolomini <alice.girolomini@studio.unibo.it>
2022-02-03 15:38:22 +0100

    Added ProductSoldOut and OrderShipped listener

---
+++ tec-web/app/Listeners/SendOrderShippedNotification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderShipped;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Models\User;
+use App\Models\Order;
+use App\Notifications\NewOrderShipped;
+
+class SendOrderShippedNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\OrderShipped  $event
+     * @return void
+     */
+    public function handle(OrderShipped $event)
+    {
+        $user=User::find($event->order->user_id);
+        $seller->notify(new SoldOut($event->order));
+    }
+}
---
+++ tec-web/app/Listeners/SendSoldOutNotification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ProductSoldOut;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Models\Product;
+use App\Models\User;
+use App\Notifications\SoldOut;
+
+class SendSoldOutNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\ProductSoldOut  $event
+     * @return void
+     */
+    public function handle(ProductSoldOut $event)
+    {
+        $seller=User::find($event->product->seller_id);
+        $seller->notify(new SoldOut($event->product));
+    }
+}
