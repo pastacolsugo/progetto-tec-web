14cf7291523b4e6280bdc8462dc7a8af8e4bb8ec
alicegirolomini <alice.girolomini@studio.unibo.it>
2022-02-09 15:01:53 +0100

    removed old nav in search view

--- tec-web/resources/views/search.blade.php
+++ tec-web/resources/views/search.blade.php
@@ -1,28 +1,14 @@
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <link rel="stylesheet" href="/css/products.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-
-<body>
+@section('content')
     <div class="container-fluid p-0 overflow-hidden">
-    <div class="row">
-        <div class="col-12">
-            <nav class="navbar bg-dark">
-                <div class="px-2">
-                    <x-select onchange="location = this.value;" class="form-select">
-                        <option value="">Sort by</option>
-                        <option value="?query=<?php echo request()->get('query')?>&sortBy=ascendingPrice" {{ (request('sortBy') == 'ascendingPrice' ? 'selected=selected' : '') }}>Low to High</option>
-                        <option value="?query=<?php echo request()->get('query')?>&sortBy=descendingPrice" {{ (request('sortBy') == 'descendingPrice' ? 'selected=selected' : '') }}>High to Low</option>
-                    </x-select>
-                </div>
-                <div class="pt-2 me-2">
-                    <x-search-form/>
-                </div>
-            </nav>
-        </div>
-    </div>
+        <x-select onchange="location = this.value;" class="form-select">
+            <option value="">Sort by</option>
+            <option value="?query=<?php echo request()->get('query')?>&sortBy=ascendingPrice" {{ (request('sortBy') == 'ascendingPrice' ? 'selected=selected' : '') }}>Low to High</option>
+            <option value="?query=<?php echo request()->get('query')?>&sortBy=descendingPrice" {{ (request('sortBy') == 'descendingPrice' ? 'selected=selected' : '') }}>High to Low</option>
+        </x-select>
+        
+            
     @if($query and $results)
         @if($results->count() > 0)
             <p class="py-2">Found {{ $results->total() }} results</p>
@@ -47,7 +33,6 @@
         <p>Search fantastic products!</p>
     @endif
     </div>
-</body>
-</html>
+@endsection
 
 
