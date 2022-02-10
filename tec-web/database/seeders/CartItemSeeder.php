3aeca11af7724059a07fce0dc24909a06b424ad5
alicegirolomini <alice.girolomini@studio.unibo.it>
2022-02-10 14:10:00 +0100

    Added CartItemSeeder

---
+++ tec-web/database/seeders/CartItemSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class CartItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $carts = DB::table('carts')->get();
+        for ($i = 0, $size = count($carts); $i < $size; $i++) {
+
+            DB::table('cart_items')->insert([
+                [
+                    'product_id'=>'1',
+                    'cart_id'=>$carts[$i]->id,
+                    'quantity'=>'2',
+                ],
+                [
+                    'product_id'=>'2',
+                    'cart_id'=>$carts[$i]->id,
+                    'quantity'=>'1',
+                ]
+            ]);
+        }
+    }
+}
