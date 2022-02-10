34a95afb6723a3216bc1e791390ff0f5851201d5
Ugo <ugobaroncini@gmail.com>
2022-02-10 17:10:21 +0100

    Revert "create component for products"
    This reverts commit 860335aad17c7f9c55459fe6afb54a04fa20f983.
    
    Cleanup of Endri's work.

--- tec-web/resources/views/components/product-card.blade.php
+++ tec-web/resources/views/components/product-card.blade.php
@@ -1,20 +1,4 @@
-<!-- <div style="border: solid black 1px" id='questoeuncomponente'>
-   
-</div> -->
-
-<div class="card card-shadow">
-    <div class="card-header card-image">
-        {{ $productName }}
-        {{ $productGallery }}
-    </div>
-    <div class="card-body">
-        <p class="card-text"> {{ $productDescription }}</p>
-        <p class="card-text">{{ $productPrice }}</p>
-    </div>
-    <div class="card-footer">
-        <button class="btn">Aggiungi al carrello</button>
-        <button class="btn">Acquista subito</button>
-    </div>
-
+<div style="border: solid black 1px" id='questoeuncomponente'>
+    <strong>{{ $productName }}</strong>
     {{ $slot }}
 </div>
