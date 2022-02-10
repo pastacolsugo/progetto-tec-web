543e5ac60221f3f05a547a1cdd770c0eee5e9820
Ugo <ugobaroncini@gmail.com>
2022-02-10 18:27:10 +0100

    Revert "some change"
    This reverts commit 0e639176e3c04d66d29383cfa42a4adc36d9ac6a.
    
    Cleanup of Endri's work.

--- tec-web/resources/views/welcome.blade.php
+++ tec-web/resources/views/welcome.blade.php
@@ -1,58 +1,53 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>eCommerce</title>
 
-    <title>eCommerce</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Google Fonts -->
+        <!-- Google Font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 
-    <!-- Google Font: open sans -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap" rel="stylesheet">
+        <!-- css -->
+        <link rel="stylesheet" href="/css/app.css">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
 
-    <!-- css -->
-    <link rel="stylesheet" href="/css/app.css">
+    <body class="antialiased">
 
-</head>
+        <!-- scprit bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-<body class="antialiased">
+        <!-- log -->
+        <!-- non so serve -->
+        <script>
+            document.querySelector('body').onclick = (e) => {
+                console.log(e);
+            }
+        </script>
 
-    <!-- scprit bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <!-- navbar -->
+        {{View::make('header')}}
+        @yield('content')
 
-    <!-- log -->
-    <!-- non so serve -->
-    <script>
-        document.querySelector('body').onclick = (e) => {
-            console.log(e);
-        }
-    </script>
+        <!-- main -->
+        {{View::make('master')}}
+        @yield('content')
 
-    <!-- navbar -->
-    {{View::make('header')}}
-    @yield('content')
-
-    <!-- main -->
-    {{View::make('master')}}
-    @yield('content')
-
-    <!-- footer -->
-    {{View::make('footer')}}
-    @yield('content')
-
-</body>
-
+        <!-- footer -->
+        {{View::make('footer')}}
+        @yield('content')
+       
+    </body>
 </html>
