9b248a70101f850084ad7eeb219f6e9812f9cbdc
Ugo <ugobaroncini@gmail.com>
2022-02-10 16:44:07 +0100

    Created POC cart page with template injecting product info

--- tec-web/resources/views/cart.blade.php
+++ tec-web/resources/views/cart.blade.php
@@ -1,5 +1,8 @@
-@extends('layouts.app')
-
-@section('content')
-    carrello
-@endsection
+<x-app-layout>
+    @foreach ($cart_items as $item)
+        <p>{{ $item->name }}</p>
+        <p>{{ $item->price }}</p>
+        <!-- TODO #a11y: alt text -->
+        <img src={{ $item->image }} alt=...>
+    @endforeach
+</x-app-layout>
