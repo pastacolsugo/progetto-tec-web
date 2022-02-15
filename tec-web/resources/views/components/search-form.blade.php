878f1fc17471b557c76bc38fde26253acde30cc5
alicegirolomini <alice.girolomini@studio.unibo.it>
2022-02-15 19:13:07 +0100

    removed csrf protection

--- tec-web/resources/views/components/search-form.blade.php
+++ tec-web/resources/views/components/search-form.blade.php
@@ -1,5 +1,4 @@
 <form action="/search" method="GET" class="d-flex">
-    @csrf
     <x-input name="query" class="form-control" value="{{ request()->get('query') }}"></x-input>
     <x-button class="btn">Search</x-button>
 </form>
