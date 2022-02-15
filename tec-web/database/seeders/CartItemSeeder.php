37c8049d73c5a01130dd25c13eed5a0682f705d7
alicegirolomini <alice.girolomini@studio.unibo.it>
2022-02-15 18:58:12 +0100

    Now CartItemSeeder updates Cart table

--- tec-web/database/seeders/CartItemSeeder.php
+++ tec-web/database/seeders/CartItemSeeder.php
@@ -15,19 +15,26 @@ class CartItemSeeder extends Seeder
     public function run()
     {
         $carts = DB::table('carts')->get();
+        $products = DB::table('products')->whereIn('id', [1, 2])->get();
         for ($i = 0, $size = count($carts); $i < $size; $i++) {
 
             DB::table('cart_items')->insert([
                 [
-                    'product_id'=>'1',
-                    'cart_id'=>$carts[$i]->id,
-                    'quantity'=>'2',
+                    'product_id' => '1',
+                    'cart_id' => $carts[$i]->id,
+                    'quantity' => '2',
                 ],
                 [
-                    'product_id'=>'2',
-                    'cart_id'=>$carts[$i]->id,
-                    'quantity'=>'1',
+                    'product_id' => '2',
+                    'cart_id' => $carts[$i]->id,
+                    'quantity' => '1',
                 ]
+            ]);
+            DB::table('carts')
+                ->where('id',$i + 1)
+                ->update([
+                    'items' => 2,
+                    'subtotal' => $products[0]->price + $products[1]->price,
             ]);
         }
     }
