5d05b416c3ac8027fe8d7aa9c3a8393245d601c7
Ugo <ugobaroncini@gmail.com>
2022-04-02 18:03:56 +0200

    [Migration] Added `model` field to Products table.

--- tec-web/database/migrations/2021_11_30_140146_create_products_table.php
+++ tec-web/database/migrations/2021_11_30_140146_create_products_table.php
@@ -16,8 +16,9 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('model')->nullable();
             $table->float('price', 8, 2);
-            $table->string('description');
+            $table->string('description', 20000);
             $table->integer('stock');
             $table->bigInteger('category_id')->unsigned();
             $table->foreign('category_id')->references('id')->on('categories');
