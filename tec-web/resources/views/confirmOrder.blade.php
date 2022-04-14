856f356861086a326d2222ab8ab20838decd5c4c
alicegirolomini <alice.girolomini@studio.unibo.it>
2022-04-14 18:17:58 +0200

    Changed confirmOrder layout

--- tec-web/resources/views/confirmOrder.blade.php
+++ tec-web/resources/views/confirmOrder.blade.php
@@ -16,9 +16,9 @@
                         </div>
                         <form action="{{ route('placeOrder') }}" method="POST">
                             @csrf
-                            <div class="mt-8 lg:w-3/4 border rounded-md max-w-md w-full px-4 py-3">
+                            <div class="mt-8 lg:w-3/4 rounded-md max-w-md w-full px-4 py-3">
                                 <h4 class="text-sm text-gray-500 font-medium">Deliver to:</h4>
-                                <div class="mt-6">
+                                <div class="mt-2">
                                     <div class="mt-2 flex">
                                         <label class="block flex-1">{{ auth()->user()->name }}</label>
                                     </div>
@@ -30,27 +30,28 @@
                                     </div>
                                 </div>
                                 <div class="mt-4">
+                                    <h4 class="text-sm text-gray-500 font-medium">Contacts:</h4>
                                     <div class="mt-2 flex">
                                     <label class="block flex-1">{{ auth()->user()->email }}</label>
                                     </div>
                                 </div>
                                 <div class="mt-6" >
                                     <h4 class="text-sm text-gray-500 font-medium">Payment method:</h4>
-                                    <div class="mt-6">
+                                    <div class="mt-2">
                                         <div class="mt-2 flex">
                                             <label class="block flex-1">ending in {{ substr(request()->card_number, -4) }}, expires {{ request()->expiration_month }}/{{ substr(request()->expiration_year,-2) }}</label>
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="transform translate-x-1/2 -translate-y-1/2 text-black peer-placeholder-shown:text-gray-300 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="transform translate-x-3/4 -translate-y-1/2 text-black peer-placeholder-shown:text-gray-300 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
                                             </svg>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="flex items-center justify-between mt-8">
-                                    <button class="flex items-center px-3 py-3 bg-blue-600 text-white text-sm font-medium rounded-md hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-                                        <span>Order now</span>
-                                    </button>
-                                </div> 
                             </div>
+                            <div class="flex items-center justify-between mt-8">
+                                <button class="flex items-center px-3 py-3 bg-amber-400 text-gray-700 text-sm font-medium rounded-md hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                                    <span>Order now</span>
+                                </button>
+                            </div> 
                         </form>
                     </div>
                     <div class="w-full mb-8 flex-shrink-0 order-1 lg:w-1/2 lg:mb-0 lg:order-2">
