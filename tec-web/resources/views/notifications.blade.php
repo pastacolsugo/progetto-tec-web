5535fec6bd9a1547194e7e3e43312222fe0cd34b
alicegirolomini <alice.girolomini@studio.unibo.it>
2022-05-26 14:46:19 +0200

    Removed white bg color from no notifications message

--- tec-web/resources/views/notifications.blade.php
+++ tec-web/resources/views/notifications.blade.php
@@ -6,34 +6,35 @@
     </x-slot>
     <div class="my-0 md:mx-0 xl:mx-8">
         <div class="py-0 align-middle inline-block min-w-full lg:py-2 sm:px-0 lg:px-8">
-            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg bg-white">
             @if($notifications)
                 @if($notifications->count() > 0)
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    <div class="order-1 mt-2 flex-shrink-0 sm:mt-0">
-                        <a href="{{ route('markNotification', $notification = "mark-all") }}" class="flex items-center justify-center px-4 py-2 shadow-sm text-sm font-medium text-gray-700 bg-amber-400"> Mark all as read </a>
-                    </div>
-                    @foreach($notifications as $notification)
-                    <div class="bg-white">
-                        <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-                            <div class="flex items-center justify-between flex-wrap">
-                                <div class="flex items-center">
-                                    <p class="ml-3 font-medium text-gray-700 truncate">
-                                        @if($notification->type == "App\Notifications\SoldOut")
-                                            <span class="md:inline"> {{$notification->data['name']}} is sold out [ {{$notification->created_at}} ] </span>
-                                        @endif
-                                        @if($notification->type == "App\Notifications\NewOrderShipped")
-                                            <span class="md:inline"> Order #{{$notification->data['name']}} has been shipped [ {{$notification->created_at}} ] </span>
-                                        @endif
-                                    </p>
-                                </div>
-                                <div class="order-2 ml-3 mt-2 flex sm:mt-0 sm:w-auto">
-                                    <a href="{{ route('markNotification', $notification->id) }}" class="flex items-center justify-center px-2 py-1 rounded text-sm font-medium text-gray-700 bg-amber-400"> Mark as read </a>
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg bg-white">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <div class="order-1 mt-2 flex-shrink-0 sm:mt-0">
+                            <a href="{{ route('markNotification', $notification = "mark-all") }}" class="flex items-center justify-center px-4 py-2 shadow-sm text-sm font-medium text-gray-700 bg-amber-400"> Mark all as read </a>
+                        </div>
+                        @foreach($notifications as $notification)
+                        <div class="bg-white">
+                            <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+                                <div class="flex items-center justify-between flex-wrap">
+                                    <div class="flex items-center">
+                                        <p class="ml-3 font-medium text-gray-700 truncate">
+                                            @if($notification->type == "App\Notifications\SoldOut")
+                                                <span class="md:inline"> {{$notification->data['name']}} is sold out [ {{$notification->created_at}} ] </span>
+                                            @endif
+                                            @if($notification->type == "App\Notifications\NewOrderShipped")
+                                                <span class="md:inline"> Order #{{$notification->data['name']}} has been shipped [ {{$notification->created_at}} ] </span>
+                                            @endif
+                                        </p>
+                                    </div>
+                                    <div class="order-2 ml-3 mt-2 flex sm:mt-0 sm:w-auto">
+                                        <a href="{{ route('markNotification', $notification->id) }}" class="flex items-center justify-center px-2 py-1 rounded text-sm font-medium text-gray-700 bg-amber-400"> Mark as read </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        @endforeach
                     </div>
-                    @endforeach
                 </div>
                 @else
                     <div class="mt-6 flex items-center justify-center">
@@ -41,7 +42,6 @@
                     </div>
                 @endif
             @endif
-            </div>
         </div>
     </div>
 </x-app-layout>
