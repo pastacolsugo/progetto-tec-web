b5a0cc9e4cd21dd321998b5b3c5e41914669e597
alicegirolomini <alice.girolomini@studio.unibo.it>
2022-05-30 12:04:39 +0200

    Now my-orders view shows order-items info

--- tec-web/resources/views/my-orders.blade.php
+++ tec-web/resources/views/my-orders.blade.php
@@ -7,32 +7,44 @@
     <div class="flex flex-col max-w-7xl mx-auto">
         <div class="my-0 overflow-x-auto md:mx-0 xl:mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-0 lg:px-8">
-            @if($orders)
-                @if($orders->count() > 0)
+            @if($orders && $order_items)
+                @if($orders->count() > 0 && $order_items->count() > 0)
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                     <table class="min-w-full divide-y divide-gray-200">
                         <thead class="bg-gray-50">
                             <tr>
-                                <th scope="col" id="order_number" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Number</th>
+                                <th scope="col" class="w-px px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden md:block" id="product_image">Image</th>
+                                <th scope="col" id="product_name" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Product</th>
+                                <th scope="col" id="order_number" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Order Number</th>
                                 <th scope="col" id="order_total" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Total</th>
                                 <th scope="col" id="order_date" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Order Date</th>
                                 <th scope="col" id="order_status" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
                             </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
-                        @foreach ($orders as $order)
+                        @foreach ($order_items as $order_item)
                             <tr>
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    <div class="text-base text-gray-900" headers="order_number">#O-0000{{ $order->id }}</div>
+                                <td class="px-6 py-4 whitespace-nowrap hidden md:table-cell" headers="product_image">
+                                    <div class="flex-shrink-0 h-20 w-20">
+                                        <a href="{{ route('product', ['id' => $order_item->product_id ]) }}">
+                                            <img class="h-20 w-20 object-cover rounded" src="{{ route('product-image', $order_item->product_id) }}" alt="{{ $order_item->name }}">
+                                        </a>
+                                    </div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
-                                    <div class="text-base font-medium text-gray-500" headers="order_total">{{ $order->order_total }}$</div>
+                                    <div class="text-base text-gray-900" headers="product_name">{{ $order_item->name }}</div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
-                                    <div class="text-base font-medium text-gray-500" headers="order_date">{{ $order->order_date }}</div>
+                                    <div class="text-base text-gray-900" headers="order_number">#O-000{{ $order_item->order_id }}</div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
-                                    <div class="text-base font-medium text-gray-500" headers="order_status">{{ $order->order_status }}</div>
+                                    <div class="text-base font-medium text-gray-500" headers="order_total">{{ $order_item->price }}$</div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-base font-medium text-gray-500" headers="order_date">{{ $orders->find($order_item->order_id)->order_date }}</div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-base font-medium text-gray-500" headers="order_status">{{ $order_item->status }}</div>
                                 </td>
                             </tr>
                         @endforeach
