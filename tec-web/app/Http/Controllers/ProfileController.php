73bfa7cc6b44a4a26fb1dfb08a6784aa47f44b42
Ugo <ugobaroncini@gmail.com>
2022-06-02 14:59:11 +0200

    Added EditProfile controller and view

---
+++ tec-web/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    public function editProfile(Request $request) {
+        $user = User::find(Auth::id())->first();
+        return view('editProfile', ['user' => $user]);
+    }
+
+    public function editProfileRequest(Request $request) {
+        if (Auth::user() == null)
+            return Response("User not logged in", 401);
+
+        $mandatoryFields = [
+            'name',
+            'email',
+            'isSeller'
+        ];
+
+        foreach ($mandatoryFields as $f) {
+            if (!$request->has($f))
+                return Response("Missing fields", 422);
+        }
+
+        $user = User::find(Auth::id())->first();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->isSeller = $request->isSeller;
+        $user->save();
+
+        return redirect()->route('dashboard');
+    }
+}
