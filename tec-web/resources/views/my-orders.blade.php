ecc255c13bc5ce90ec5a1669042b4be59c20a884
Ugo <ugobaroncini@gmail.com>
2022-06-03 02:29:44 +0200

    Improved code readability by switching if-else

--- tec-web/resources/views/my-orders.blade.php
+++ tec-web/resources/views/my-orders.blade.php
@@ -7,59 +7,59 @@
     <div class="flex flex-col max-w-7xl mx-auto">
         <div class="my-0 overflow-x-auto md:mx-0 xl:mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-0 lg:px-8">
-            @if($orders && $order_items)
-                @if($orders->count() > 0 && $order_items->count() > 0)
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                            <tr>
-                                <th scope="col" class="w-px px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden md:block" id="product_image">Image</th>
-                                <th scope="col" id="product_name" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Product</th>
-                                <th scope="col" id="order_number" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Order Number</th>
-                                <th scope="col" id="unit_price" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Unit Price</th>
-                                <th scope="col" id="product_quantity" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Quantity</th>
-                                <th scope="col" id="product_total" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Product Total</th>
-                                <th scope="col" id="order_date" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Order Date</th>
-                                <th scope="col" id="status" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-                        @foreach ($order_items as $order_item)
-                            <tr>
-                                <td class="px-6 py-4 hidden md:block">
-                                    <div class="flex-shrink-0 h-20 w-20 flex flex-col justify-center">
-                                        <img class="max-h-20 max-w-[5rem] object-contain" src="{{ route('product-image', [$order_item->product_id]) }}" alt="">
-                                    </div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap" headers="product_name">
-                                    <div class="text-base text-gray-900">{{ $order_item->name }}</div>
-                                <td class="px-6 py-4 whitespace-nowrap" headers="order_number">
-                                    <div class="text-base text-gray-900">{{ sprintf("#%05d", $order_item->order_id) }}</div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap" headers="unit_price">
-                                    <div class="text-base font-medium text-gray-500">{{ $order_item->price }} €</div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap" headers="product_quantity">
-                                    <div class="text-base font-medium text-gray-500">{{ $order_item->quantity }}x</div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap" headers="product_total">
-                                    <div class="text-base font-medium text-gray-500">{{ $order_item->price * $order_item->quantity }} €</div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap" headers="order_date">
-                                    <div class="text-base font-medium text-gray-500">{{ $orders->find($order_item->order_id)->order_date }}</div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap" headers="status">
-                                    <div class="text-base font-medium text-gray-500">{{ $order_item->status }}</div>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
+            @if ($orders && $order_items)
+                @if ($orders->count() <= 0 || $order_items->count() <= 0)
+                    <div class="mt-6 flex items-center justify-center">
+                        <p class="text-gray-700 text-2xl font-medium">Your order history is empty</p>
+                    </div>
                 @else
-                <div class="mt-6 flex items-center justify-center">
-                    <p class="text-gray-700 text-2xl font-medium">Your order history is empty</p>
-                </div>
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col" class="w-px px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider hidden md:block" id="product_image">Image</th>
+                                    <th scope="col" id="product_name" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Product</th>
+                                    <th scope="col" id="order_number" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Order Number</th>
+                                    <th scope="col" id="unit_price" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Unit Price</th>
+                                    <th scope="col" id="product_quantity" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Quantity</th>
+                                    <th scope="col" id="product_total" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Product Total</th>
+                                    <th scope="col" id="order_date" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Order Date</th>
+                                    <th scope="col" id="status" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                            @foreach ($order_items as $order_item)
+                                <tr>
+                                    <td class="px-6 py-4 hidden md:block">
+                                        <div class="flex-shrink-0 h-20 w-20 flex flex-col justify-center">
+                                            <img class="max-h-20 max-w-[5rem] object-contain" src="{{ route('product-image', [$order_item->product_id]) }}" alt="">
+                                        </div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap" headers="product_name">
+                                        <div class="text-base text-gray-900">{{ $order_item->name }}</div>
+                                    <td class="px-6 py-4 whitespace-nowrap" headers="order_number">
+                                        <div class="text-base text-gray-900">{{ sprintf("#%05d", $order_item->order_id) }}</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap" headers="unit_price">
+                                        <div class="text-base font-medium text-gray-500">{{ $order_item->price }} €</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap" headers="product_quantity">
+                                        <div class="text-base font-medium text-gray-500">{{ $order_item->quantity }}x</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap" headers="product_total">
+                                        <div class="text-base font-medium text-gray-500">{{ $order_item->price * $order_item->quantity }} €</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap" headers="order_date">
+                                        <div class="text-base font-medium text-gray-500">{{ $orders->find($order_item->order_id)->order_date }}</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap" headers="status">
+                                        <div class="text-base font-medium text-gray-500">{{ $order_item->status }}</div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 @endif
             @endif
             </div>
