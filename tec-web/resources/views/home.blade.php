22cff3403b426c8b378d1c822ec8117be8cb2b0e
alicegirolomini <alice.girolomini@studio.unibo.it>
2022-06-04 10:07:53 +0200

    Changed <b> to <strong> tag

--- tec-web/resources/views/home.blade.php
+++ tec-web/resources/views/home.blade.php
@@ -11,7 +11,7 @@
                         <x-slot name="name">{{ $products[$i]->name }}</x-slot>
                     </x-product-card>
                 @endfor
-                <div class="col-span-full my-4 py-2 shadow-lg bg-gray-600 text-gray-100 text-center">SCONTONI su <b>Localhost!</b></div>
+                <div class="col-span-full my-4 py-2 shadow-lg bg-gray-600 text-gray-100 text-center">SCONTONI su <strong>Localhost!</strong></div>
                 @for ($i = 8; $i < 16; $i++)
                     <x-product-card>
                         <x-slot name="id">{{ $products[$i]->id }}</x-slot>
@@ -19,7 +19,7 @@
                         <x-slot name="name">{{ $products[$i]->name }}</x-slot>
                     </x-product-card>
                 @endfor
-                <div class="col-span-full my-4 py-2 shadow-lg bg-gray-600 text-gray-100 text-center">SCONTONI su <b>Localhost!</b></div>
+                <div class="col-span-full my-4 py-2 shadow-lg bg-gray-600 text-gray-100 text-center">SCONTONI su <strong>Localhost!</strong></div>
                 @for ($i = 16; $i < 24; $i++)
                     <x-product-card>
                         <x-slot name="id">{{ $products[$i]->id }}</x-slot>
