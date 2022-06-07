632734e621bfcdd9c055b131dde31eac79caf396
Ugo <ugobaroncini@gmail.com>
2022-06-08 01:22:20 +0200

    Fix product display in confirm order.

--- tec-web/resources/views/confirmOrder.blade.php
+++ tec-web/resources/views/confirmOrder.blade.php
@@ -54,27 +54,43 @@
                             </div>
                         </form>
                     </div>
-                    <div class="w-full mb-8 flex-shrink-0 order-1 lg:w-1/2 lg:mb-0 lg:order-2">
+                    <div class="w-full mb-8 flex-shrink-0 order-1 lg:w-1/2 lg:mb-0 lg:order-2 overflow-y-auto">
                         <div class="flex justify-center lg:justify-end">
                             <div class="border rounded-md max-w-md w-full px-4 py-3">
-                            <div class="flex items-center justify-between">
+                                <div class="flex items-center justify-between">
                                     <h3 class="text-gray-700 font-medium">Totale ({{ $cart->items }}): </h3>
-                                    <h3 class="text-gray-700 text-sm">{{ $cart->subtotal }} €</h3>
+                                    <h3 class="text-gray-700 text-sm">{{ sprintf("%.2f €", $cart->subtotal) }}</h3>
                                 </div>
-                                @foreach($cart_items as $cart_item)
-                                    <div class="flex justify-between mt-6">
-                                        <div class="flex">
-                                            <img class="h-20 w-20 object-cover rounded" src="{{ route('product-image', $cart_item->product_id) }}" alt="{{ $cart_item->name }}">
-                                            <div class="mx-3">
-                                                <h3 class="text-sm text-gray-600">{{ $cart_item->name }}</h3>
-                                                <div class="flex items-center mt-2">
-                                                    <span class="text-gray-700 mx-2"> {{ $cart_item->quantity }} </span>
-                                                </div>
+                                <div class="my-4 grid grid-cols-[2fr_3fr_2fr] gap-y-6 gap-x-4">
+                                    @foreach($cart_items as $cart_item)
+                                        <div class="h-20 w-20 flex flex-col justify-center">
+                                            <img class="max-h-20 max-w-[5rem] object-contain rounded" src="{{ route('product-image', $cart_item->product_id) }}" alt="{{ $cart_item->name }}">
+                                        </div>
+                                        <div class="flex flex-col justify-around">
+                                            <h3 class="text-sm text-gray-600">{{ $cart_item->name }}</h3>
+                                            <div class="flex items-center mt-2">
+                                                <form action="{{ route('addProductToCart') }}" method="POST">
+                                                @csrf
+                                                    <x-input type="hidden" name="product_id" value="{{ $cart_item->product_id }}"/>
+                                                    <x-input type="hidden" name="quantity" value="1"/>
+                                                    <button type="submit" class="text-gray-500 focus:outline-none focus:text-gray-600">
+                                                        <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                                    </button>
+                                                </form>
+                                                <span class="text-gray-700 mx-2"> {{ $cart_item->quantity }} </span>
+                                                <form action="{{ route('removeProductFromCart') }}" method="POST">
+                                                @csrf
+                                                    <x-input type="hidden" name="product_id" value="{{ $cart_item->product_id }}"/>
+                                                    <x-input type="hidden" name="quantity" value="1"/>
+                                                    <button type="submit" class="btn text-gray-500 focus:outline-none focus:text-gray-600">
+                                                        <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                                    </button>
+                                                </form>
                                             </div>
                                         </div>
-                                        <span class="text-gray-600">{{ $cart_item->price }} €</span>
-                                    </div>
-                                @endforeach
+                                        <span class="text-gray-600 text-right">{{ sprintf("%.2f €", $cart_item->price * $cart_item->quantity) }}</span>
+                                    @endforeach
+                                </div>
                             </div>
                         </div>
                     </div>
