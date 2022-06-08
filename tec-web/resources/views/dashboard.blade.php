244db12885eadec9c09e8c2d354f3c9aba2e9e31
Ugo <ugobaroncini@gmail.com>
2022-06-08 02:12:47 +0200

    Fixed buttons.

--- tec-web/resources/views/dashboard.blade.php
+++ tec-web/resources/views/dashboard.blade.php
@@ -8,13 +8,15 @@
     <div class="py-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-4 md:gap-x-8 lg:gap-8 lg:gap-x-16 jusify-between p-6 bg-white border-b border-gray-200">
-                    @if ($isSeller)
-                      <x-dashboard-button href="{{ route('sellerListing') }}">I miei annunci</x-dashboard-button>
-                      <x-dashboard-button href="{{ route('createProduct') }}">Crea nuovo annuncio</x-dashboard-button>
-                      <x-dashboard-button href="{{ route('seller-orders-listing') }}">Ordini clienti</x-dashboard-button>
-                    @endif
+                @if ($isSeller)
+                    <div class="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-4 md:gap-x-8 lg:gap-8 lg:gap-x-16 jusify-between p-6 pb-0 lg:pb-2 bg-white">
+                        <x-dashboard-button href="{{ route('sellerListing') }}">I miei annunci</x-dashboard-button>
+                        <x-dashboard-button href="{{ route('createProduct') }}">Crea nuovo annuncio</x-dashboard-button>
+                        <x-dashboard-button href="{{ route('seller-orders-listing') }}">Ordini clienti</x-dashboard-button>
+                    </div>
+                @endif
 
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-x-12 lg:gap-8 lg:gap-x-16 jusify-between p-6 bg-white border-b border-gray-200">
                     <x-dashboard-button href="{{ route('notifications') }}">
                         <span class="mr-2 items-center justify-center">Notifiche</span>
                         @if(auth()->user()->unreadNotifications)
