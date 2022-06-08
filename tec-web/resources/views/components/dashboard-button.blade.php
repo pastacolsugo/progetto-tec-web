a5e248c80f8a1de8b51f39d37047e2d109dd71a8
Ugo <ugobaroncini@gmail.com>
2022-06-08 02:01:39 +0200

    Fixed button spacing.

--- tec-web/resources/views/components/dashboard-button.blade.php
+++ tec-web/resources/views/components/dashboard-button.blade.php
@@ -1,1 +1,1 @@
-<a {{ $attributes->merge(['class' => 'py-2 px-4 bg-white border border-gray-200 text-gray-600 rounded hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex justify-center items-center text-center']) }}>{{ $slot }}</a>
+<a {{ $attributes->merge(['class' => 'py-2 px-4 bg-white border border-gray-300 text-gray-600 rounded hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex justify-center items-center text-center']) }}>{{ $slot }}</a>
--- tec-web/resources/views/dashboard.blade.php
+++ tec-web/resources/views/dashboard.blade.php
@@ -8,23 +8,20 @@
     <div class="py-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="grid grid-cols-3 gap-4 gap-x-8 lg:gap-8 lg:gap-x-16 jusify-between p-6 bg-white border-b border-gray-200">
-                    {{-- TODO: Localize strings! --}}
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-4 md:gap-x-8 lg:gap-8 lg:gap-x-16 jusify-between p-6 bg-white border-b border-gray-200">
                     @if ($isSeller)
-                      {{-- <a href="{{ route('sellerListing') }}" class="py-2 px-4 bg-white border border-gray-200 text-gray-600 rounded hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex justify-center items-center">Listino</a> --}}
                       <x-dashboard-button href="{{ route('sellerListing') }}">I miei annunci</x-dashboard-button>
                       <x-dashboard-button href="{{ route('createProduct') }}">Crea nuovo annuncio</x-dashboard-button>
                       <x-dashboard-button href="{{ route('seller-orders-listing') }}">Ordini clienti</x-dashboard-button>
                     @endif
 
-                    {{-- TODO: Add links and replace placeholder buttons --}}
                     <x-dashboard-button href="{{ route('notifications') }}">
                         <span class="mr-2 items-center justify-center">Notifiche</span>
-                    @if(auth()->user()->unreadNotifications)
-                        @if(auth()->user()->unreadNotifications->count() > 0)
-                        <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">{{ auth()->user()->unreadNotifications->count() }}</span>
+                        @if(auth()->user()->unreadNotifications)
+                            @if(auth()->user()->unreadNotifications->count() > 0)
+                                <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">{{ auth()->user()->unreadNotifications->count() }}</span>
+                            @endif
                         @endif
-                    @endif
                     </x-dashboard-button>
                     <x-dashboard-button href="{{ route('cart') }}">Carrello</x-dashboard-button>
                     <x-dashboard-button href="{{ route('my-orders') }}">Ordini</x-dashboard-button>
