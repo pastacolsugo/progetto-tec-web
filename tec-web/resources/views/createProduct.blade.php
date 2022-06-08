caabc493f767efac999599c24165cfd2b4be1319
Ugo <ugobaroncini@gmail.com>
2022-06-08 02:20:34 +0200

    Fixed padding.

--- tec-web/resources/views/createProduct.blade.php
+++ tec-web/resources/views/createProduct.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-<div class="max-w-7xl mx-auto bg-white py-8 my-4 border border-gray-200">
-<div class="my-0 overflow-x-auto md:mx-0 xl:mx-8">
+<div class="max-w-7xl mx-auto bg-white pb-8 pt-4 md:py-8 my-4 border border-gray-200 rounded">
+<div class="mx-4 md:mx-0 my-0 overflow-x-auto md:mx-0 xl:mx-8">
     <div class="flex justify-between">
         <h1 class="font-medium text-3xl">Crea un nuovo prodotto</h1>
     </div>
@@ -9,13 +9,9 @@
         @csrf
         <input type="hidden" name="product_id" id="product_id" value=""/>
         <div class="mt-8 grid lg:grid-cols-2 gap-8 gap-x-16">
-            <div>
-                <label class="text-sm text-gray-700 block mb-1 font-medium">Immagine</label>
-            </div>
-
-            <div>
+            <div class="col-span-2">
                 <label for="upload" class="text-sm text-gray-700 block mb-1 font-medium">Carica una nuova immagine</label>
-                <input class="py-8" type="file" accept=".jpeg,.jpg,.png,.webp" name="image_upload" id="upload" required="required">
+                <input class="py-4" type="file" accept=".jpeg,.jpg,.png,.webp" name="image_upload" id="upload" required="required">
             </div>
 
             <div>
@@ -42,7 +38,7 @@
                 </x-select>
             </div>
 
-            <div class="col-span-full">
+            <div class="col-span-2">
                 <label for="description" class="text-sm text-gray-700 block mb-1 font-medium">Descrizione</label>
                 <textarea cols="50" rows="8" name="description" id="description" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full" placeholder=""></textarea>
             </div>
